b9b893ffa8640e15f7c777cc04620c65f22a38fc
MathiasSchr <saihtam.08@orange.fr>
2024-01-09 14:57:39 +0100

    Modif Gen_json

--- Generateur_json_config.py
+++ Generateur_json_config.py
@@ -40,22 +40,23 @@ Creation du dictionnaire vide
 """
 
 config = {
-     Nom_AS1 : { "Nombre_routeur" : N1 , "Matrice_adjacence" : M1 , "Matrice_adressage" : [["","",""],["","",""],["","",""]] },
-     Nom_AS2 : { "Nombre_routeur" : N2 , "Matrice_adjacence" : M2 , "Matrice_adressage" : [["","",""],["","",""],["","",""]] }
+     Nom_AS1 : { "Nombre_routeur" : N1 , "Matrice_adjacence" : M1 , "Masque_reseau" : "" , "Matrice_adressage" : [["","",""],["","",""],["","",""]] },
+     Nom_AS2 : { "Nombre_routeur" : N2 , "Matrice_adjacence" : M2 , "Masque_reseau" : "" , "Matrice_adressage" : [["","",""],["","",""],["","",""]] }
 }
 
 """ 
-Fonction Adressage_AS(Nom_As ; Nombre_routeur) --> None
-Configure les adresses des routeurs d'une AS dans le fichier json 
+Fonction Adressage_AS(Nom_As , Matrice_adjacence , Nombre_routeur) --> None
+Configure les masques reseau et les adresses des liens d'une AS dans le fichier json
 """
 def Adressage_AS(Nom_AS , Matrice_adjacence, Nombre_routeur) :
+        config[Nom_AS]["Masque_reseau"] = Nom_AS+"::"+"0"+"/40"
         for i in range(Nombre_routeur) :
             for j in range(Nombre_routeur) :
                 if Matrice_adjacence[i][j] :
-                     addresse_unique1 = Nom_AS+"."+str(i)+"::"+"1"
-                     addresse_unique2 = Nom_AS+"."+str(i)+"::"+"2"
-                     config[Nom_AS]["Matrice_adressage"][i][j] = addresse_unique1
-                     config[Nom_AS]["Matrice_adressage"][j][i] = addresse_unique2
+                     adresse_unique1 = config[Nom_AS]["Masque_reseau"][:3]+"."+str(i+1)+"::"+"1/64"
+                     adresse_unique2 = config[Nom_AS]["Masque_reseau"][:3]+"."+str(i+1)+"::"+"2/64"
+                     config[Nom_AS]["Matrice_adressage"][i][j] = adresse_unique1
+                     config[Nom_AS]["Matrice_adressage"][j][i] = adresse_unique2
                      
 
 """
