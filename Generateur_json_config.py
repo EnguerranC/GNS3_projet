5d37f81b5c8a232db6848c05f39ef83f885caeeb
MathiasSchr <saihtam.08@orange.fr>
2024-01-17 17:18:45 +0100

    m

--- Generateur_json_config.py
+++ Generateur_json_config.py
@@ -36,7 +36,7 @@ N2 = 3
 #numeros des routers dans l'AS :
 L2 = [4,5,6]
 # Protocole interne
-Protocole_AS2 = "OSPF" 
+Protocole_AS2 = "OSPF"
 # Graphe d'adjacence de taille N2xN2
 M2 = [[0,1,0],[1,0,1],[0,1,0]]
 # Masque reseau interface physique
@@ -52,20 +52,15 @@ A rajouter plus tard :
 """
 
 # Generateur de la matrice d'adressage vide
-M_ad1 = []
-for i in range(N1) :
-   M_temp = []
-   for j in range(N1) :
-      M_temp.append("")
-   M_ad1.append(M_temp)
-
-M_ad2 = []
-for i in range(N2) :
-   M_temp = []
-   for j in range(N2) :
-      M_temp.append("")
-   M_ad2.append(M_temp)
-
+def Matrice_addressage_vide(M_ad, N) :
+   for i in range(N) :
+      M_temp = []
+      for j in range(N) :
+         M_temp.append(["",""])
+      M_ad.append(M_temp)
+M_ad1, M_ad2 = [], []
+Matrice_addressage_vide(M_ad1, N1)
+Matrice_addressage_vide(M_ad2, N2)
 """
 Creation du dictionnaire vide
 """
@@ -75,7 +70,7 @@ config = {
       "Nombre_routeur":N1,
       "Matrice_adjacence":M1,
       "Masque_reseau":Masque1,
-      "Matrice_adressage":M_ad1,
+      "Matrice_adressage_interface": M_ad1,
       "Donnees_routeurs":{
       },
       "Routage_intraAS":{
@@ -85,7 +80,7 @@ config = {
       "Routage_interAS":{
          Num_routeur_bordeur1 : {
             Num_AS2 : {
-               "Protocole" : "BGP"
+               "Protocole" : "BGP",
                "Adresse": ""
          }
          }
@@ -95,7 +90,7 @@ config = {
       "Nombre_routeur":N2,
       "Matrice_adjacence":M2,
       "Masque_reseau":Masque2,
-      "Matrice_adressage":M_ad2,
+      "Matrice_adressage_interface":M_ad2,
       "Donnees_routeurs":{     
       },
       "Routage_intraAS":{
@@ -105,7 +100,7 @@ config = {
       "Routage_interAS":{
          Num_routeur_bordeur2 : {
             Num_AS1 : {
-               "Protocole" : "BGP"
+               "Protocole" : "BGP",
                "Adresse": ""
          }
          }
@@ -125,21 +120,26 @@ for i in range(1,3) :
 Fonction Adressage_AS(Nom_As , Matrice_adjacence , Nombre_routeur) --> None
 Configure les adresses des liens d'une AS dans le fichier json
 """
-def Adressage_AS(Num_AS , Matrice_adjacence, Nombre_routeur) :
+def Adressage_AS(Num_AS , Matrice_adjacence, Nombre_routeur, M_adresse) :
+        nb_connexions = [0 for i in range(Nombre_routeur)] 
         for i in range(Nombre_routeur) :
             for j in range(Nombre_routeur) :
-                if Matrice_adjacence[i][j] :
-                     adresse_unique1 = config[Num_AS]["Masque_reseau"][:3]+":0:0:"+str(i+1)+"::"+"1/64"
-                     adresse_unique2 = config[Num_AS]["Masque_reseau"][:3]+":0:0:"+str(i+1)+"::"+"2/64"
-                     config[Num_AS]["Matrice_adressage"][i][j] = adresse_unique1
-                     config[Num_AS]["Matrice_adressage"][j][i] = adresse_unique2
+               if Matrice_adjacence[i][j] :
+                  nb_connexions[i] += 1
+                  nb_connexions[j] += 1
+                  adresse_unique1 = config[Num_AS]["Masque_reseau"][:3]+":0:0:"+str(j+1)+"::"+"1/64"
+                  adresse_unique2 = config[Num_AS]["Masque_reseau"][:3]+":0:0:"+str(j+1)+"::"+"2/64"
+                  interface1 = "GigabitEthernet"+str(nb_connexions[i])+"/0"
+                  interface2 = "GigabitEthernet"+str(nb_connexions[i])+"/0"
+                  config[Num_AS]["Matrice_adressage_interface"][i][j] = [adresse_unique1, interface1]
+                  config[Num_AS]["Matrice_adressage_interface"][j][i] = [adresse_unique2, interface2]
                      
 
 """
 Programme principal
 """
-Adressage_AS(Num_AS1,M1,N1) # Adressage de l'AS1
-Adressage_AS(Num_AS2,M2,N2) # Adressage de l'AS2
+Adressage_AS(Num_AS1,M1,N1,M_ad1) # Adressage de l'AS1
+Adressage_AS(Num_AS2,M2,N2,M_ad2) # Adressage de l'AS2
 
 fichier = open("config.json","w") # Creation du fichier json
 json.dump(config, fichier, indent=4)
